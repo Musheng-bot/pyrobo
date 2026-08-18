9f7c3abb84584df22667b948cca4329a9aebc381
meish <meishenghan685@hotmail.com>
2026-08-18 15:22:38 +0800

    feat: del astar codes

--- planner/astar.py
+++
@@ -1,8 +0,0 @@
-from sim.map import Map
-from sim.simulator import Simulator
-from .planner import PathPlanner
-
-
-class AStar(PathPlanner):
-    def __init__(self, robo_map: Map, sim: Simulator) -> None:
-        super().__init__(robo_map, sim)
