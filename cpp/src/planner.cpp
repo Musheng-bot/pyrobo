1805a21b884f4bc170a7cd764890cd4473e5674e
meish <meishenghan685@hotmail.com>
2026-08-25 17:09:50 +0800

    add planner.cpp back

--- cpp/src/planner.cpp
+++ cpp/src/planner.cpp
@@ -3,18 +3,14 @@
 namespace pyrobo {
 
 Path PathPlanner::plan(
-    const Pose& robot_pose,
-    const Pose& goal_pose,
-    const Map& map
+    const Pose& robot_pose, const Pose& goal_pose, const Map& map
 ) {
     // Question 1: implement route planning here.
     return Path{{robot_pose.x, robot_pose.y}, {goal_pose.x, goal_pose.y}};
 }
 
 std::pair<float, float> ControlPlanner::control_plan(
-    const Pose& robot_pose,
-    const Pose& goal_pose,
-    const Map& map,
+    const Pose& robot_pose, const Pose& goal_pose, const Map& map,
     const Feedback& fdb
 ) {
     // Question 3: implement control planning here.
